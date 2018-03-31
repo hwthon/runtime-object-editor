02202878cd5a1fb044c57a24c8336cfe4f239b63
Hirofumi Watanabe <hwthon.depp@gmail.com>
2018-03-31 17:55:29 +0900

    Field Prototype.

--- Assets/App/Scripts/ModelEditor/Viewer.cs
+++ Assets/App/Scripts/ModelEditor/Viewer.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+// using UnityEngine.UI;
 
 namespace SerializableModelEditor
 {
 	public class Viewer : MonoBehaviour
 	{
+		[SerializeField]
+		private Transform _root;
+
 		public void Show(object model)
 		{
+			foreach (FieldInfo fieldInfo in model.GetType().GetFields())
+			{
+				if (fieldInfo.FieldType == typeof(string))
+				{
+					GameObject field = Instantiate(Resources.Load<GameObject>("Fields/Field"));
 
+					field.transform.SetParent(_root);
+				}
+			}
 		}
 
 		private void Update()
@@ -29,4 +42,4 @@ objectのメンバーを辿っていく
 
 
 
-*/ 
+*/
