f23ea436ae67da2b7b0f88fe64f4af545b58830d
Hirofumi Watanabe <hwthon.depp@gmail.com>
2018-04-01 01:03:44 +0900

    メソッドわけた

--- Assets/App/Scripts/ModelEditor/ModelEditor.cs
+++ Assets/App/Scripts/ModelEditor/ModelEditor.cs
@@ -53,41 +53,59 @@ namespace SerializableModelEditor
 		/// </summary>
 		private void OnGUI()
 		{
-			if (!_enable)
+			if (!_enable || _objectViewer == null)
 			{
 				return;
 			}
-
-			if (_objectViewer == null)
-			{
-				return;
-			}
-
-			float headerHeight = Screen.height / 10f;
 
 			using(new GUILayout.AreaScope(new Rect(0f, 0f, Screen.width, Screen.height), string.Empty, GUI.skin.box))
 			{
-				using(new GUILayout.AreaScope(new Rect(0f, 0f, Screen.width, headerHeight)))
+				DrawScreen();
+			}
+		}
+
+		private void DrawScreen()
+		{
+			float headerHeight = Screen.height / 10f;
+
+			using(new GUILayout.AreaScope(new Rect(0f, 0f, Screen.width, headerHeight)))
+			{
+				DrawHeader();
+			}
+
+			using(new GUILayout.AreaScope(new Rect(0f, headerHeight, Screen.width, Screen.height - headerHeight)))
+			using(new GUILayout.VerticalScope(GUI.skin.box))
+			{
+				DrawFields();
+			}
+		}
+
+		/// <summary>
+		/// ヘッダーの描画
+		/// </summary>
+		private void DrawHeader()
+		{
+			using(new GUILayout.HorizontalScope(GUI.skin.box))
+			{
+				GUILayout.Label("ModelEditor", GUILayout.ExpandWidth(true));
+
+				if (GUILayout.Button("x", GUILayout.ExpandWidth(false)))
 				{
-					using(new GUILayout.HorizontalScope(GUI.skin.box))
-					{
-						GUILayout.Label("ModelEditor", GUILayout.ExpandWidth(true));
-
-						if (GUILayout.Button("x", GUILayout.ExpandWidth(false)))
-						{
-							Close();
-						}
-					}
+					Close();
 				}
+			}
+		}
 
-				using(new GUILayout.AreaScope(new Rect(0f, headerHeight, Screen.width, Screen.height - headerHeight)))
-				using(new GUILayout.VerticalScope(GUI.skin.box))
-				using(var scrollScope = new GUILayout.ScrollViewScope(_scrollPosition))
-				{
-					_objectViewer.OnGUI();
+		/// <summary>
+		/// オブジェクトのフィールドの描画
+		/// </summary>
+		private void DrawFields()
+		{
+			using(var scrollScope = new GUILayout.ScrollViewScope(_scrollPosition))
+			{
+				_objectViewer.OnGUI();
 
-					_scrollPosition = scrollScope.scrollPosition;
-				}
+				_scrollPosition = scrollScope.scrollPosition;
 			}
 		}
 
