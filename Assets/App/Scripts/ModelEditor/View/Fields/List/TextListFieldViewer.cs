b911aeb203a2f199b8985e05244fd68a7d4c9d0b
Hirofumi Watanabe <hwthon.depp@gmail.com>
2018-04-01 15:57:26 +0900

    string.

--- Assets/App/Scripts/ModelEditor/View/Fields/List/TextListFieldViewer.cs
+++ Assets/App/Scripts/ModelEditor/View/Fields/List/TextListFieldViewer.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 
 namespace SerializableModelEditor
 {
-	public class TextListFieldViewer : FieldViewer
+	public class TextListFieldViewer : ListFieldViewer<string>
 	{
-		private bool _foldout = true;
-
 		public TextListFieldViewer(object model, FieldInfo fieldInfo) : base(model, fieldInfo) { }
 
 		public static bool Available(System.Type type)
@@ -16,67 +14,9 @@ namespace SerializableModelEditor
 			return type == typeof(List<string>);
 		}
 
-		public override void OnGUI()
+		protected override string DrawItemField(string value)
 		{
-			if (_model == null || _fieldInfo == null)
-			{
-				return;
-			}
-
-			using(new FieldScope(false))
-			{
-				if (GUILayout.Button(_foldout ? "▼" : "▶︎", GUILayout.ExpandWidth(false)))
-				{
-					_foldout = !_foldout;
-				}
-				GUILayout.Label(_model.GetType().Name);
-			}
-
-			if (_foldout)
-			{
-				using(new GUILayout.HorizontalScope())
-				using(new IndentScope(View.indentLevel + 1))
-				using(new FieldScope(false))
-				{
-					using(new GUILayout.VerticalScope())
-					{
-						System.Type itemType = typeof(string);
-						System.Type listType = typeof(List<>).MakeGenericType(itemType);
-
-						var list = _fieldInfo.GetValue(_model) as List<string>;
-						if (list == null)
-						{
-							list = new List<string>();
-						}
-
-						int removeIndex = -1;
-						for (int i = 0; i < list.Count; i++)
-						{
-							using(new GUILayout.HorizontalScope())
-							{
-								list[i] = GUILayout.TextField(list[i]);
-
-								if (GUILayout.Button("-", GUILayout.ExpandWidth(false)))
-								{
-									removeIndex = i;
-								}
-							}
-						}
-
-						if (removeIndex != -1)
-						{
-							list.RemoveAt(removeIndex);
-						}
-
-						if (GUILayout.Button("+"))
-						{
-							list.Add(default(string));
-						}
-
-						_fieldInfo.SetValue(_model, list);
-					}
-				}
-			}
+			return GUILayout.TextField(value);
 		}
 	}
 }
